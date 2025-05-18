e7a1f02268d92c590b9b5f04ec4eca8a5feb579d
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2025-05-18 11:31:31 -0300

    Add files via upload

---
+++ lista_compras_alura/scripts/criarItemDaLista.js
@@ -0,0 +1,44 @@
+import gerarDiaDaSemana from "./gerarDiaDaSemana.js";
+
+const inputItem = document.getElementById("input-item");
+let contador = 0;
+export function criarItemDaLista(){
+    
+    if(inputItem.value === ''){
+        alert("Por favor, insira um item!");
+        return;
+    }
+
+    const itemDaLista = document.createElement("li"); //criação de item da lista
+    const containerItemDaLista  = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    inputCheckBox.addEventListener("click", function(){
+        if(inputCheckBox.checked){
+            nomeItem.style.textDecoration = "line-through";
+        }else {
+            nomeItem.style.textDecoration = "none";
+        }
+    })
+
+
+    containerItemDaLista.appendChild(inputCheckBox);
+    containerItemDaLista.appendChild(nomeItem);
+    itemDaLista.appendChild(containerItemDaLista);
+
+    const dataCompleta = gerarDiaDaSemana();
+
+    
+    const itemData = document.createElement("p");
+    itemData.innerHTML = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+    
+    
+    return itemDaLista;
+}
